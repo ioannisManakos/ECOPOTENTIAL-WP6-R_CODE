1ab0eadcb6816f8eaf506dc22c13f0c230577f2b
ioannisManakos <imanakos@iti.gr>
2016-03-24 09:52:19 +0200

    Update run.R

--- src/main/app-resources/node/run.R
+++ src/main/app-resources/node/run.R
@@ -2,32 +2,27 @@ library(rgdal)
 library(raster)
 library(pixmap)
 library(bmp)
+library(RCurl)
+library(bitops)
 
 rm(list=ls())
 
-setwd("C:\\Users\\kordelas\\Downloads\\rasterLayers_tif\\rasterLayers_tif\\example\\")
-download.file(url = 'https://github.com/GeoScripting-WUR/IntroToRaster/raw/gh-pages/data/gewata.zip', destfile = 'gewata.zip', method = 'auto')
+download.file(url='https://dl.dropboxusercontent.com/s/l81f5tyijq25sz6/gewata.zip', destfile='gewata.zip',method='curl')
+unzip("gewata.zip",exdir="./")
 
-unzip('gewata.zip')
+gewata <- brick('LE71700552001036SGS00_SR_Gewata_INT1U.tif')
 
-#gewata <- brick('LE71700552001036SGS00_SR_Gewata_INT1U.tif')
-gewata <- brick('C:\\Users\\kordelas\\Desktop\\Lufa\\Merged_Tiff2.tif')
-
-gewata_sub<- gewata[1:1000,1:1000,4]
-dim(gewata_sub) <- c(1000, 1000,4)
-
-ndvi <- (gewata_sub[,,4] - gewata_sub[,,3]) / (gewata_sub[,,4] + gewata_sub[,,3])
+ndvi <- (gewata[,,4] - gewata[,,3]) / (gewata[,,4] + gewata[,,3])
 
 thres=0.4
 
 M2 <- ndvi
-
 M2[ndvi < thres] <- 0
-M2[ndvi >= thres] <- 1 
+M2[ndvi >= thres] <- 1
 
-R_1<-gewata_sub[,,2]
-G_1<-gewata_sub[,,1]
-B_1<-gewata_sub[,,3]
+R_1<-gewata[,,2]
+G_1<-gewata[,,1]
+B_1<-gewata[,,3]
 
 R_1[ndvi<thres]<-0
 G_1[ndvi<thres]<-0
@@ -40,4 +35,4 @@ B_raster<-raster(B_1)
 rr <- stack(R_raster,G_raster,B_raster)
 
 write.table(M2, file="mymatrix.txt", row.names=FALSE, col.names=FALSE)
-writeRaster(rr, filename='ndvi10.tif',format="GTiff",overwrite=TRUE)
+writeRaster(rr, filename='ndvi10.tif',Format="GTiff",overwrite=TRUE)
